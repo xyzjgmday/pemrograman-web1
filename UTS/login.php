66d278c2b8c30b14254d34e084f1b6b1b0a3a2d4
xyzjgmday <daysubhiz@gmail.com>
2023-05-13 20:35:53 +0700

    logout

--- UTS/login.php
+++ UTS/login.php
@@ -2,20 +2,21 @@
 session_start();
 
 // Check if the email and password are correct
-if($_POST['email'] == 'admin@example.com' && $_POST['password'] == 'password') {
-	// Set the user as logged in
-	$_SESSION['is_logged_in'] = true;
-	// Redirect to the home page
-	header('Location: home.php');
-	exit();
+if ($_POST['email'] == 'admin@example.com' && $_POST['password'] == 'password') {
+    // Set the user as logged in and store the email in the session
+    $_SESSION['is_logged_in'] = true;
+    $_SESSION['user_email'] = $_POST['email'];
+    // Redirect to the home page
+    header('Location: home.php');
+    exit();
 } else {
-	// Set the login error based on the type of error
-	if($_POST['email'] != 'admin@example.com') {
-		$_SESSION['login_error'] = 'email_not_found';
-	} else {
-		$_SESSION['login_error'] = 'password_wrong';
-	}
-	// Redirect back to the login page
-	header('Location: index.php');
-	exit();
+    // Set the login error based on the type of error
+    if ($_POST['email'] != 'admin@example.com') {
+        $_SESSION['login_error'] = 'email_not_found';
+    } else {
+        $_SESSION['login_error'] = 'password_wrong';
+    }
+    // Redirect back to the login page
+    header('Location: index.php');
+    exit();
 }
