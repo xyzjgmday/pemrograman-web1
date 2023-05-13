341acc033f49134cbf2746b0ec5fcf962c594cad
xyzjgmday <daysubhiz@gmail.com>
2023-05-13 20:46:21 +0700

    edit

--- UTS/home.php
+++ UTS/home.php
@@ -1,48 +1,25 @@
-<?php
-session_start();
-
-// Check if the user is logged in
-if(!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
-	// Redirect back to the login page
-	header('Location: index.php');
-	exit();
-}
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Home Page</title>
+    <title>Login Success</title>
     <!-- bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 
 <body>
-    <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">My Website</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Page 1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Page 2</a>
-                </li>
-            </ul>
+    <!-- Success Message -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h1 class="text-center mt-5 mb-4">Login Success!</h1>
+                <p class="text-center">Welcome, <?php echo $_SESSION['email']; ?>.</p>
+                <div class="text-center">
+                    <a href="logout.php" class="btn btn-danger">Logout</a>
+                </div>
+            </div>
         </div>
-        <span class="navbar-text">
-            Welcome, <?php echo $_SESSION['user_email']; ?>
-            <a href="logout.php">Logout</a>
-        </span>
-    </nav>
+    </div>
 
     <!-- bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
